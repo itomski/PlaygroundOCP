d9da6b340f997697094832983fca2195d70c8b07
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2023-06-06 13:10:15 +0200

    Update

---
+++ src/main/java/de/lubowiecki/oca/playground/lambdas/Aufgabe.java
@@ -0,0 +1,23 @@
+package de.lubowiecki.oca.playground.lambdas;
+
+public class Aufgabe {
+
+    public static void main(String[] args) {
+
+        /*
+
+        Schreibe 3 Lambda-Implementierungen für jedes der Interfaces
+        1 x Wrapper
+        1 x primitiv
+        1 x anderen Komplexen (nicht Wrapper z.B. Person)
+
+        Consumer
+        Supplier
+        Function
+        UnaryOperator
+        Predicate
+
+        */
+
+    }
+}
